276b7e11ce89f45a365ffff27b5bfcdb3b6fbb76
krerati56 <s68120226103mail.kmutnb.ac.th>
2025-07-23 14:21:02 +0700

    work

--- codingw05-02.c
+++ codingw05-02.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 
 int main() {
-char name[30];
-int age;
-float hight;
-char subject;
-float grade;
-char gradechar;
+    char name[30];       // ชื่อ
+    int age;             // อายุ
+    float height;        // ส่วนสูง
+    char subject;        // รหัสวิชา เช่น M
+    float grade;         // เกรด เช่น 3.5
+    char gradechar;      // ตัวอักษรแทนเกรด เช่น A
 
+    printf("Enter your name, age, height, subject, grade, gradechar: ");
+    scanf("%s %d %f %c %f %c", name, &age, &height, &subject, &grade, &gradechar);
 
-printf("Enter your name, age, hight, subject, grade, gradechar: ");
-scanf("%c %d %f %c %f %c", &name, &age, &hight, &subject, &grade, &gradechar);
+    printf("%s is %d years old and %.1f centimeters tall.\n", name, age, height);
+    printf("In subject %c, grade %.2f which is symbolized as '%c'.\n", subject, grade, gradechar);
 
-
-printf("%s is %d years old and %.1f centimeters tall.\n", name, age, hight);
-printf("In subject %c, grade %.2f which is symbolized as '%c'.\n", subject, grade, gradechar);
-
+    return 0;
 }
